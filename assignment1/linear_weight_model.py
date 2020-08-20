4925395c37dc98af5a7da7559b57ff488b215887
Zantiki <55238744+Zantiki@users.noreply.github.com>
2020-08-20 11:26:44 +0200

    Added 3d plotting and other fixes

--- assignment1/linear_weight_model.py
+++ assignment1/linear_weight_model.py
@@ -19,20 +19,35 @@ class LinearRegressionModel:
         return x @ self.W + self.b
 
     # Uses Mean Squared Error
-    def loss(self, x, y, z):
+    def loss(self, x, y):
         return torch.mean(torch.square(self.f(x) - y))
 
 
 class LinearRegression3D:
 
     def __init__(self):
-        self.W = np.array([[0.0], [0.0]])
-        self.b = np.array([[0.0]])
+        self.W = torch.tensor([[0.0], [0.0]], requires_grad=True)
+        self.b = torch.tensor([[0.0]], requires_grad=True)
 
     def f(self, x):
         return x @ self.W + self.b
 
-    def loss(self, x, y, z):
+    def loss(self, x, y):
+        return torch.mean(torch.square(self.f(x) - y))
+
+
+class NonLinearRegressionModel:
+    def __init__(self):
+        # requires_grad enables calculation of gradients
+        self.W = torch.tensor([[0.0]], requires_grad=True)
+        self.b = torch.tensor([[0.0]], requires_grad=True)
+
+    # Predictor
+    def f(self, x):
+        return 20*torch.sigmoid(x @ self.W + self.b) + 31
+
+    # Uses Mean Squared Error
+    def loss(self, x, y):
         return torch.mean(torch.square(self.f(x) - y))
 
 
@@ -70,22 +85,21 @@ def get_data(three_dimensions=False):
 
         x_packed = []
 
-        while len(x_list) > 0:
-            x_packed.append([x_list.pop(), z_list.pop()])
-        x_train = torch.tensor(x_packed).reshape(-1, 2)
-        print("x_train: ", x_train)
-        return x_train, y_train
+        """while len(x_list) > 0:
+            x_packed.append([x_list.pop(), y_list.pop()])"""
+        x_train = torch.tensor([[length, weight] for length, weight in zip(x_list, y_list)])
+        z_train = torch.tensor(z_list)
+        return x_train, z_train
     else:
         x_train = torch.tensor(x_list).reshape(-1, 1)
         return x_train, y_train
 
 
-def train(model, x, y, z):
-
-    learning_rate = 0.0001
+def train(model, x, y):
+    learning_rate = 0.00001
     optimizer = torch.optim.SGD([model.b, model.W], learning_rate)
-    for epoch in range(12000):
-        model.loss(x, y, z).backward()
+    for epoch in range(120000):
+        model.loss(x, y).backward()
         optimizer.step()
         optimizer.zero_grad()
     return model
@@ -98,100 +112,46 @@ def plot2D(model, x_train, y_train):
     x = torch.tensor([[torch.min(x_train)], [torch.max(x_train)]])  # x = [[1], [6]]]
     plt.plot(x, model.f(x).detach(), label='$y = f(x) = xW+b$')
     plt.legend()
-    print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train, None)))
+    print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
     plt.show()
 
 
-def plot3D(model, x, y):
-    fig = plt.figure('Linear regression: 3D')
-    x_train = x.numpy()
-    y_train = y.numpy()
-    plot1 = fig.add_subplot(111, projection='3d')
-    plot1.plot(x_train[:, 0].squeeze(),
-               x_train[:, 1].squeeze(),
-               y_train[:, 0].squeeze(),
-               'o',
-               label='$(\\hat x_1^{(i)}, \\hat x_2^{(i)},\\hat y^{(i)})$',
-               color='blue')
+def plot_3D_alt(model, x_train, y_train):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    plot_info = fig.text(0.01, 0.02, '')
+    y_list = []
+    x_list = []
 
-    plot1_f = plot1.plot_wireframe(np.array([[]]), np.array([[]]), np.array([[]]), color='green',
-                                  label='$y = f(x) = xW+b$')
+    for i in x_train:
+        x_list.append(i[0].detach())
+        y_list.append(i[1].detach())
 
-    plot1_info = fig.text(0.01, 0.02, '')
-
-    plot1_loss = []
-    for i in range(0, x_train.shape[0]):
-        line, = plot1.plot([0, 0], [0, 0], [0, 0], color='red')
-        plot1_loss.append(line)
-        if i == 0:
-            line.set_label('$|f(\\hat x^{(i)})-\\hat y^{(i)}|$')
-
-    plot1.set_xlabel('$x_1$')
-    plot1.set_ylabel('$x_2$')
-    plot1.set_zlabel('$y$')
-    plot1.legend(loc='upper left')
-    plot1.set_xticks([])
-    plot1.set_yticks([])
-    plot1.set_zticks([])
-    plot1.w_xaxis.line.set_lw(0)
-    plot1.w_yaxis.line.set_lw(0)
-    plot1.w_zaxis.line.set_lw(0)
-    plot1.quiver([0], [0], [0], [np.max(x_train[:, 0] + 1)], [0], [0], arrow_length_ratio=0.05, color='black')
-    plot1.quiver([0], [0], [0], [0], [np.max(x_train[:, 1] + 1)], [0], arrow_length_ratio=0.05, color='black')
-    plot1.quiver([0], [0], [0], [0], [0], [np.max(y_train[:, 0] + 1)], arrow_length_ratio=0.05, color='black')
-    update_figure(x_train, y_train, plot1, plot1_loss, plot1_info, fig)
+    z_list = model.f(x_train).detach()
+    ax.scatter(x_list, y_list, z_list, label="Trained")
+    ax.scatter(x_list, y_list, y_train, label="Actual")
+    ax.set_xlabel("     Length as cm")
+    ax.set_ylabel("     Weight as kg")
+    ax.set_zlabel("     Age as days")
+    ax.legend()
+    plot_info.set_text(
+        '$W=\\left[\\stackrel{%.2f}{%.2f}\\right]$\n$b=[%.2f]$\n$loss = %.2f$' %
+        (model.W[0, 0], model.W[1, 0], model.b[0, 0], model.loss(x_train, y_train)))
+    print("W = %s, b = %s, loss = %s" % (model.W, model.b, model.loss(x_train, y_train)))
     plt.show()
-
-
-def update_figure(x_train, y_train, plot1, plot1_loss, plot1_info, fig, event=None):
-
-    if event is not None:
-        if event.key == 'W':
-            model.W[0, 0] += 0.01
-        elif event.key == 'w':
-            model.W[0, 0] -= 0.01
-        elif event.key == 'E':
-            model.W[1, 0] += 0.01
-        elif event.key == 'e':
-            model.W[1, 0] -= 0.01
-
-        elif event.key == 'B':
-            model.b[0, 0] += 0.05
-        elif event.key == 'b':
-            model.b[0, 0] -= 0.05
-
-    global plot1_f
-    x1_grid, x2_grid = np.meshgrid(np.linspace(1, 6, 10), np.linspace(1, 4.5, 10))
-    y_grid = np.empty([10, 10])
-    for i in range(0, x1_grid.shape[0]):
-        for j in range(0, x1_grid.shape[1]):
-            y_grid[i, j] = model.f([[x1_grid[i, j], x2_grid[i, j]]])
-    plot1_f = plot1.plot_wireframe(x1_grid, x2_grid, y_grid, color='green')
-
-    for i in range(0, x_train.shape[0]):
-        plot1_loss[i].set_data(np.array([x_train[i, 0], x_train[i, 0]]), np.array([x_train[i, 1], x_train[i, 1]]))
-        plot1_loss[i].set_3d_properties(np.array([y_train[i, 0], model.f(x_train[i, :])], dtype=object))
-
-    fig.canvas.draw()
-
 
 
 if __name__ == "__main__":
 
-    # 3D
-    x, y = get_data(three_dimensions=True)
-    print("X: ", x)
-    model = LinearRegression3D()
-    plot3D(model, x, y)
-    exit()
-    # 2D
-    get_data()
-    model = LinearRegressionModel()
+    # A: 2D
+    """model = LinearRegressionModel()
     x, y = get_data()
-    model = train(model, x, y, None)
+    train(model, x, y)
     plot2D(model, x, y)
+    input("Move to assignment B?")"""
 
-    # 3D
-   # data = get_data()
-    # model = train(data)
-    # plot(model)
+    # B: 3D
+    x, y = get_data(three_dimensions=True)
+    model = LinearRegression3D()
+    train(model, x, y)
+    plot_3D_alt(model,  x, y)
