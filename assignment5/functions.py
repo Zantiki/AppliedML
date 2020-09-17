204d675d9576435147d9b57cf5430d0739b16581
sebastian <darealzantiki@gmail.com>
2020-09-17 12:35:18 +0200

    added data-cleaning and some meaningful visualization

--- assignment5/functions.py
+++ assignment5/functions.py
@@ -1,4 +1,6 @@
+import pandas
 from pandas import DataFrame as df
+import pandas
 import matplotlib.pyplot as plt
 import itertools
 
@@ -41,7 +43,6 @@ def build_feature_dict(filename):
 
 
 def build_pandas(feature_dict, data_file):
-    main_table = df()
     with open(data_file, "r") as file:
         line = file.readline()
         data_dict = {}
@@ -57,10 +58,8 @@ def build_pandas(feature_dict, data_file):
                 feature_name = feature["name"]
                 frame_dict[feature_name] = feature["class_map"][classification]
             data_dict[dict_index] = frame_dict
-            # print(df.from_dict({0: data_dict}))
             line = file.readline()
             dict_index += 1
-            # print(main_table)
         main_table = df.from_dict(data_dict).T
     return main_table
 
@@ -85,10 +84,16 @@ if __name__ == "__main__":
 
         label1 = "population"
         label2 = "habitat"
-        #label2 = "habitat"
         feature_dict = build_feature_dict("data/cleaned_features.txt")
         data_frames = build_pandas(feature_dict, "data/agaricus-lepiota.data")
-        print(data_frames)
         x, y, amounts = get_overlap_amount(data_frames, label1, label2)
-        plt.scatter(x=x, y=y, s=amounts)
-        plt.show()
+        # plt.scatter(x=x, y=y, s=amounts)
+        # plt.show()
+        dummies = pandas.get_dummies(data_frames)
+        print(dummies.describe())
+
+        plt.spy(dummies.T, markersize=0.02, c="red")
+        fig = plt.gcf()
+        fig.set_size_inches(100, 3)
+        plt.plot()
+        plt.savefig("space.png")
